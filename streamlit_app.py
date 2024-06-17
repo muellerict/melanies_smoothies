dc97f7e107240bfcdbf7c2b07c386c6e45cc2d2a
muellerict <155457847+muellerict@users.noreply.github.com>
2024-06-17 07:58:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Create Your Smoothie :cup_with_straw:")
@@ -44,8 +45,10 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
-# New section to display fruityvice nutritional information
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+if ingredientslists:
+    ingredients_string = ''
+
+for fruit_chosen in ingredients_list:
+    ingredients_string += fruit_chosen + ' '
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
